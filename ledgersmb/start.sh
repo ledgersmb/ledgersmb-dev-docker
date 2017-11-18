1fb4415dc403b3de07a6167b99ac517bbde87bba
Erik Huelsmann <ehuels@gmail.com>
2017-11-18 23:11:47 +0100

    * Create 'master' development container

---
+++ ledgersmb/start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+update_ssmtp.sh
+cd /srv/ledgersmb
+
+if [[ ! -f ledgersmb.conf ]]; then
+  cp conf/ledgersmb.conf.default ledgersmb.conf
+  sed -i \
+    -e "s/\(cache_templates = \).*\$/cache_templates = 1/g" \
+    -e "s/\(host = \).*\$/\1$POSTGRES_HOST/g" \
+    -e "s/\(port = \).*\$/\1$POSTGRES_PORT/g" \
+    -e "s/\(default_db = \).*\$/\1$DEFAULT_DB/g" \
+    -e "s%\(sendmail   = \).*%\1/usr/sbin/ssmtp%g" \
+    /srv/ledgersmb/ledgersmb.conf
+fi
+
+# Currently unmaintained/untested
+# if [ ! -z ${CREATE_DATABASE+x} ]; then
+#   perl tools/dbsetup.pl --company $CREATE_DATABASE \
+#   --host $POSTGRES_HOST \
+#   --postgres_password "$POSTGRES_PASS"
+#fi
+
+# start ledgersmb
+exec starman --port 5762 tools/starman.psgi
