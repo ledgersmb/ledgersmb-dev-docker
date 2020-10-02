af849ca64fda34dc19afeb711d6a9de2940fa44f
Erik Huelsmann <ehuels@gmail.com>
2020-10-02 11:45:05 +0200

    Enable more logging in the database container
    Since these are development images, we want more logging, to help debugging the database layer more easily.

--- postgres/lsmb-dev-init.sh
+++ postgres/lsmb-dev-init.sh
@@ -3,8 +3,11 @@
 
 pgdata=${PGDATA:-/var/lib/postgresql/data}
 
-sed -i -e '
+sed -i -e "
 s/^#fsync = on/fsync = off/;
 s/^#synchronous_commit = on/synchronous_commit = off/;
-' $pgdata/postgresql.conf
+s/^#log_statement = 'none'/log_statement = 'all'/;
+s/^#log_min_duration_statement = -1/log_min_duration_statement = 0/;
+s/^#log_duration = off/log_duration = on/;
+" $pgdata/postgresql.conf
 
